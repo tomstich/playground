62cfe9745a59e54af18a4099619104620811bac3
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-08-03 11:16:04 +0200

    add modify type function
    function with two arguments and control structure

--- types.php
+++ types.php
@@ -3,10 +3,15 @@
 $integer = 42;
 $string = 'Hallo';
 
-function type($var) {
-  echo gettype($var) . "\n";
+function type($var , $return)
+{
+    if ($return == true) {
+        return gettype($var);
+    } else {
+        echo gettype($var) . "\n";
+    }
 }
 
-type($integer);
-type($string);
-type(25.5);
+echo type($integer , true);
+type($string , false);
+echo type(25.5 , true);
