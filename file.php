5cc29c158687c8f4a799047be6aba9b7aeaf1d9c
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-08-04 14:17:27 +0200

    Add error messages

--- file.php
+++ file.php
@@ -1,18 +1,26 @@
 <?php
 
-// Wenn Datei nicht existiert, dann erstellen wir sie
-// Datei öffnen
-// Zu speichernde Daten aus Argument einlesen
-define('FILENAME', 'file.txt');
+require 'fileutils.php';
 
-if (!file_exists(FILENAME)) {
-    echo FILENAME . ' does not exist!' . "\n" . 'Creating the File' . "\n";
+/*
+ * $ php file.php <filename> [<input>]
+ */
+
+$errorMessage = "usage: php file.php <filename> [<input>]\n";
+
+if (isset($argv[1])) {
+    $fileName = $argv[1];
+} else {
+    echo $errorMessage;
+    exit(1);
 }
-$handle = fopen(FILENAME , "w+");
 
-$somecontent = 'Hund,Hase,Maus';
-
-if (fwrite($handle, $somecontent) === FALSE) {
-    echo "Cannot write to file ($filename)";
-    exit;
+if (isset($argv[2])) {
+    $content = $argv[2];
+} else {
+    echo $errorMessage;
+    exit(2);
 }
+
+
+writeContent($content, $fileName);
