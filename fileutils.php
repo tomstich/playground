16d4052f592bdfd29aea2d2d79c7d2cdf8b40664
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2016-08-04 14:19:07 +0200

    Add fileutils.php and functions

---
+++ fileutils.php
@@ -0,0 +1,36 @@
+<?php
+
+function writeContent($content, $fileName)
+{
+    $handle = openFile($fileName);
+    $res = fwrite($handle, $content);
+
+    fclose($handle);
+
+    if ($res === false) {
+        echo "Cannot write to $fileName\n";
+        exit;
+    }
+}
+
+function checkForFile($fileName)
+{
+    $fileExists = file_exists($fileName);
+
+    if (!$fileExists) {
+        echo $fileName . ' does not exist!' . "\n" . 'Creating the File' . "\n";
+    }
+}
+
+function openFile($fileName)
+{
+    checkForFile($fileName);
+
+    $handle = fopen($fileName , "w+");
+
+    if (!$handle) {
+        echo 'Could not open file!' . "\n";
+        exit;
+    }
+    return $handle;
+}
