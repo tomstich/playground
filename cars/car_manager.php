05d65214c604092a877489fc84967694a7bd1901
Hauke Stange <hauke@jimdo.com>
2016-08-07 12:37:04 +0200

    Extract car managing functions
    In order have a clear distinction between the car managing code and its
    usage I put functions in their own file.

--- cars/car_manager.php
+++ cars/car_manager.php
@@ -1,39 +1,5 @@
 <?php
 
-/*
-Wheels
-Current speed
-Max speed
-Seats
-Color
-horsepower
-status: driving, parking
-brand
-*/
-
-$wheels = 4;
-$currentSpeed = 0.0;
-$maxSpeed = 220.0;
-$seats = 5;
-$color = 'black';
-$horsepower = 110;
-$status = 'parking';
-$brand = 'BMW';
-
-$currentSpeed = accelerate($currentSpeed, 50, $maxSpeed);
-$currentSpeed = accelerate($currentSpeed, 50, $maxSpeed);
-$currentSpeed = accelerate($currentSpeed, 50, $maxSpeed);
-$currentSpeed = accelerate($currentSpeed, 50, $maxSpeed);
-$currentSpeed = accelerate($currentSpeed, 50, $maxSpeed);
-$currentSpeed = accelerate($currentSpeed, 50, $maxSpeed);
-
-$currentSpeed = decelerate($currentSpeed, 50);
-$currentSpeed = decelerate($currentSpeed, 50);
-$currentSpeed = decelerate($currentSpeed, 50);
-$currentSpeed = decelerate($currentSpeed, 50);
-$currentSpeed = decelerate($currentSpeed, 50);
-$currentSpeed = decelerate($currentSpeed, 50);
-
 function accelerate($currentSpeed, $increase, $maxSpeed)
 {
     $oldSpeed = $currentSpeed;
