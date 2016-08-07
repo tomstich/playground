16298ac63523e7c18c22434eb12852efd5bc4c79
Hauke Stange <hauke@jimdo.com>
2016-08-07 16:01:06 +0200

    A hell of an exercise :-D

--- cars/car_manager.php
+++ cars/car_manager.php
@@ -1,5 +1,39 @@
 <?php
 
+function start(string $status): string
+{
+    return $status = 'running';
+}
+
+function stop(string $status): string
+{
+    return $status = 'parking';
+}
+
+function drive(float $aMileage, float $aCurrentSpeed, float $aMaxSpeed, float $aIncrease, float $aDistance): array
+{
+    $mileage = $aMileage + $aDistance;
+    $currentSpeed = accelerate($aCurrentSpeed, $aIncrease, $aMaxSpeed);
+    return [$mileage, $currentSpeed];
+}
+
+function obsolescence(string $aBrand): float
+{
+    if ($aBrand === 'BMW') {
+        return 5000.0;
+    }
+
+    if ($aBrand === 'Mercedes') {
+        return 7500.0;
+    }
+
+    if ($aBrand === 'VW') {
+        return 3200.0;
+    }
+
+    return 4000.0;
+}
+
 function accelerate($currentSpeed, $increase, $maxSpeed)
 {
     $oldSpeed = $currentSpeed;
