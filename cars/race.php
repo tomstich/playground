6ac0f7b8b6234cbe908515effd825eaba873cf81
Tom Stich <tom.stich@jimdo.com>
2016-08-09 10:56:36 +0200

    Add race for testing

---
+++ cars/race.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'car_manager.php';
+
+$myCar = createCar('BMW');
+$myCar = start($myCar);
+$myCar = drive($myCar, 80, 3700);
+$myCar = stop($myCar);
+
+$brands = ['BMW', 'Mercedes', 'VW', 'Audi', 'Renault', 'Tesla', 'Porsche'];
+
+// Create the fleet
+$fleet = [];
+$fleetSize = 100;
+
+for ($i=0; $i < $fleetSize; $i++) {
+    $car = createCar($brands[rand(0, count($brands) - 1)]);
+
+    // [
+    //     'brand' => 'BMW',
+    //     'maxSpeed => 222.0, // every brand should have a different max speed
+    //     'identifier' => 'GFGFDEDGDGDFGSDHTSHSTHSER' // something unique
+    // ]
+
+    // 'identifier' is a unique name! (hint: see `uniqid` in PHP docs)
+    $fleet[$car['identifier']] = $car;
+}
+
+// Start fleet
+foreach ($fleet as $identifier => $car) {
+    $fleet[$identifier] = start($car);
+}
+
+$allCarsDead = false;
+
+while (!$allCarsDead) {
+
+    $allCarsDead = true;
+
+    foreach ($fleet as $identifier => $car) {
+        $fleet[$identifier] = drive($car, 80, 100);
+        if ($fleet[$identifier]['status'] !== 'broken') {
+            $allCarsDead = false;
+        }
+    }
+}
